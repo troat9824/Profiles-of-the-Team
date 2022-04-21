123ff30ff634dcdf6ee0a5682ce035109acaba0b
troat9824 <troat9824@gmail.com>
2022-04-20 23:17:16 -0500

    engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,80 @@
+const { test } = require('@jest/globals');
 const { TestWatcher } = require('jest');
+const Engineer = require('../lib/Engineer')
+
+test("creates engineer name", () => {
+    const engineerName = "Taylor";
+    const testEngineer = new Engineer(engineerName);
+    expect(testEngineer.name).toBe(engineerName)
+
+});
+
+
+test("creates engineer id", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const testEngineer = new Engineer(engineerName,engineerId);
+    expect(testEngineer.id).toBe(engineerId)
+
+});
+
+test("checking engineer's email", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const engineerEmail = "Taylor@gmail.com"
+    const testEngineer = new Engineer(engineerName,engineerId,engineerEmail);
+    expect(testEngineer.email).toBe(engineerEmail)
+
+});
+
+test("checking engineer's gitHub", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const engineerEmail = "Taylor@gmail.com"
+    const engineerGithub = "taylor.github"
+    const testEngineer = new Engineer(engineerName,engineerId,engineerEmail,engineerGithub);
+    expect(testEngineer.engineerGithub).toBe(engineerGithub)
+})
+
+test("checking engineer's getName", () => {
+    const engineerName = "Taylor";
+    const testEngineer = new Engineer(engineerName);
+    expect(testEngineer.getName()).toBe(engineerName)
+
+});
+
+
+test("checking engineer's getId", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const testEngineer = new Engineer(engineerName,engineerId);
+    expect(testEngineer.getId()).toBe(engineerId)
+
+});
+
+test("checking engineer's getEmail", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const engineerEmail = "Taylor@gmail.com"
+    const testEngineer = new Engineer(engineerName,engineerId,engineerEmail);
+    expect(testEngineer.getEmail()).toBe(engineerEmail)
+
+});
+
+test("checking engineer's getRole", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const engineerEmail = "Taylor@gmail.com"
+    const testEngineer = new Engineer(engineerName,engineerId,engineerEmail);
+    expect(testEngineer.getRole()).toBe("Engineer")
+
+});
+
+test("checking engineer's gitHub", () => {
+    const engineerName = "Taylor";
+    const engineerId ="ABC123"
+    const engineerEmail = "Taylor@gmail.com"
+    const engineerGithub = "taylor.github"
+    const testEngineer = new Engineer(engineerName,engineerId,engineerEmail,engineerGithub);
+    expect(testEngineer.getGithub()).toBe(engineerGithub)
+});
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,62 @@
 const { TestWatcher } = require('jest');
+const Intern = require('../lib/Intern')
+
+test("creates intern name", () => {
+    var internName = "Taylor";
+    const testIntern = new Intern(internName);
+    expect(testIntern.name).toBe(internName)
+
+});
+
+
+test("creates intern id", () => {
+    var internName = "Taylor";
+    var internId ="ABC123"
+    const testIntern = new Intern(internName,internId);
+    expect(testIntern.id).toBe(internId)
+
+});
+
+test("checking intern's email", () => {
+    var internName = "Taylor";
+    var internId ="ABC123"
+    var internEmail = "Taylor@gmail.com"
+    const testIntern = new Intern(internName,internId,internEmail);
+    expect(testIntern.email).toBe(internEmail)
+
+});
+
+
+test("checking intern's getName", () => {
+    var internName = "Taylor";
+    const testIntern = new Intern(internName);
+    expect(testIntern.getName()).toBe(internName)
+
+});
+
+
+test("checking intern's getId", () => {
+    var internName = "Taylor";
+    var internId ="ABC123"
+    const testIntern = new Intern(internName,internId);
+    expect(testIntern.getId()).toBe(internId)
+
+});
+
+test("checking intern's getEmail", () => {
+    var internName = "Taylor";
+    var internId ="ABC123"
+    var internEmail = "Taylor@gmail.com"
+    const testIntern = new Intern(internName,internId,internEmail);
+    expect(testIntern.getEmail()).toBe(internEmail)
+
+});
+
+test("checking intern's getRole", () => {
+    var internName = "Taylor";
+    var internId ="ABC123"
+    var internEmail = "Taylor@gmail.com"
+    const testIntern = new Intern(internName,internId,internEmail);
+    expect(testIntern.getRole()).toBe("intern")
+
+});
